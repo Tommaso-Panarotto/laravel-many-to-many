5d1bb83296544b6834bcfd5e55229167ed406c68
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-07 15:40:40 +0100

    projectseeder modify with faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,15 +14,16 @@ class ProjectSeeder extends Seeder
      * Run the database seeds.
      */
 
-
-    public function run(): void
+    public function run(Faker $faker): void
     {
+        $typeIds = Type::all()->pluck("id");
+
         $project = new Project();
         $project->title = "BoolFlix";
         $project->author = "Tommaso-Panarotto";
         $project->url = "https://github.com/Tommaso-Panarotto/vite-boolflix";
         $project->description = "Simulazione di una libreria stile netfl";
-        $project->type_id = 1;
+        $project->type_id = $faker->randomElement($typeIds);
         $project->save();
     }
 }
