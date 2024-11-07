8f83a10c7c2881b83866dd5b8ad89f3d5f1bde50
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-07 16:02:22 +0100

    migration projecttechnology table relation

--- database/migrations/2024_11_07_143901_create_project_technology_table.php
+++ database/migrations/2024_11_07_143901_create_project_technology_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId("project_id")->constrained();
+            $table->foreignId("technology_id")->constrained();
+            $table->primary(["project_id", "technology_id"]);
         });
     }
 
