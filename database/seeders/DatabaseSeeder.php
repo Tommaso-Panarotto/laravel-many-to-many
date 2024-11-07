f3ef25a0162568aabf54aa517826f9d6755a0891
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-07 16:03:13 +0100

    added seeder ProjectTechnologySeeder to databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypeSeeder::class,
             TechnologySeeder::class,
-            ProjectSeeder::class
+            ProjectSeeder::class,
+            ProjectTechnologySeeder::class
         ]);
     }
 }
