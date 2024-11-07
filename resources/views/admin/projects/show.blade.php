4049e99d01d169770a5e92155fd30194aa05eb09
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-07 16:59:12 +0100

    technlogies used in detail page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,6 +7,11 @@
                 <h2 class="card-title">{{ $project->title }}</h2>
                 <h6 class="card-subtitle mb-2 text-body-secondary">{{ $project->author }}</h6>
                 <div class="type">{{ $project->type->name }}</div>
+                <div class="technologie">
+                    @foreach ($project->technologies as $technology)
+                        <span class="ms-3">{{ $technology->name }}</span>
+                    @endforeach
+                </div>
                 <p class="card-text">{{ $project->description }}</p>
                 <div class="mb-4"><a href="{{ $project->url }}" class="btn btn-primary card-link text-white">link
                         progetto</a></div>
